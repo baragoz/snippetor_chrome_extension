22e9f353882588c8036a20d6a60216f50cbebd47
umlsynco <evgeny.alexeyev@gmail.com>
2017-02-03 01:21:50 +0300

    Dropped base64 usage. Getting data directly from extension.

--- dist/chrome/background.js
+++ dist/chrome/background.js
@@ -2,9 +2,6 @@
 
 var cachedTabs = [];
 
-// Create Base64 Object
-var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}};
-
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status !== 'loading') return;
 
@@ -81,6 +78,7 @@ console.log("DRAFT ID == " + workingEnvironment[sender.tab.id]);
       this._broadcastTabs(sender.tab.id, "onSnippetChange", {action: "save", working: workingEnvironment[sender.tab.id]});
       // TODO: action depends on preferences; we could add UID only on save
       snippetsList[workingEnvironment[sender.tab.id]] = null;
+      workingEnvironment[sender.tab.id] = null;
       return true;
     },
     onRemoveSnippetDraft: function(payload) {
@@ -103,6 +101,7 @@ console.log("DRAFT ID == " + workingEnvironment[sender.tab.id]);
         chrome.tabs.create({'url': payload.items[0].url + "#L"+payload.items[0].line, active:true}, function(tab) {
           workingEnvironment[tab.id] = pos;
         });
+        this._broadcastTabs(-1, "onOpenSnippet", {action: "open", working: workingEnvironment[sender.tab.id], items: payload.items});
         sendRes({status:"opening"});
         return true;
       }
@@ -116,10 +115,7 @@ console.log("DRAFT ID == " + workingEnvironment[sender.tab.id]);
       var snipettorURL =  "http://localhost:8000";
       var pos = workingEnvironment[sender.tab.id];
       if (pos != undefined && pos >= 0) {
-        var encodedString = Base64.encode(JSON.stringify(snippetsList[pos]));
-        chrome.tabs.create({'url': snipettorURL + "?save="+encodedString, active:true}, function(tab) {
-          console.log("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
-          console.dir(arguments);
+        chrome.tabs.create({'url': snipettorURL + "?save=draft", active:true}, function(tab) {
           // Assign opened tab as snippet handler
           workingEnvironment[tab.id] = pos;
         });
